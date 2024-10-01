e8e87d3474a50a4d4b5602243472d406a5e48e44
Naxisigut <834525217@qq.com>
2024-10-01 09:25:16 +0800

    feat: numSanitize

--- src/number/index.ts
+++ src/number/index.ts
@@ -1,3 +1,5 @@
+export * from './numSanitize'
+
 /**
  * 获取min~max之间的随机整数
  * @param min 最小数
--- test/number.test.ts
+++ test/number.test.ts
@@ -1,6 +1,7 @@
-import { test, expect } from 'vitest';
+import { test, expect, describe, it } from 'vitest';
 import { add, sub, mul, div, isInteger } from '../src/number/index';
 import { zeroPadPre, getRandomNumber, getRandomNumberFactory } from '../src/number/index';
+import { numSanitize } from '../src/number/index';
 
 test('add', () => {
   expect(add(1, 2, 3, 4)).toBe(10)  // 两数加法 => 多数求和
@@ -49,4 +50,110 @@ test('getRandomNumberFactory', () => {
     expect(func()).toSatisfy(isInteger)
   }
 })
+
+
+
+describe('numSanitize', () => {
+  it('happy path', () => {
+    expect(numSanitize('-123abc456-.`*()')).toBe('123456')
+
+    expect(numSanitize('-123abc', { 
+      isMinusAllowed: true, 
+    })).toBe('-123')
+
+    expect(numSanitize('0.123', { 
+      isDotAllowed: true, 
+    })).toBe('0.123')
+
+    expect(numSanitize('0.123', { 
+      isDotAllowed: true, 
+      digits: 2
+    })).toBe('0.12')
+  })
+  
+  it('normal edge case', () => {
+    expect(numSanitize('0123')).toBe('123')
+
+    expect(numSanitize('000123')).toBe('123')
+  })
+
+  it('minus edge case', () => {
+    expect(numSanitize('--123', {
+      isMinusAllowed: true
+    })).toBe('-123')
+
+    expect(numSanitize('--123--', {
+      isMinusAllowed: true
+    })).toBe('-123')
+  })
+
+  it('dot edge case', () => {
+    expect(numSanitize('0..123', { 
+      isDotAllowed: true, 
+    })).toBe('0.123')
+
+    expect(numSanitize('0..123..', { 
+      isDotAllowed: true, 
+    })).toBe('0.123')
+
+    expect(numSanitize('.12', { 
+      isDotAllowed: true, 
+    })).toBe('12') // 不允许首位直接输入小数点
+
+    expect(numSanitize('.', { 
+      isDotAllowed: true, 
+    })).toBe('')
+  })
+
+  it('digit edge case', () => {
+    expect(numSanitize('0.123', { 
+      isDotAllowed: true, 
+      digits: 0
+    })).toBe('0.1')
+
+    expect(numSanitize('0.123', { 
+      isDotAllowed: true, 
+      digits: -1
+    })).toBe('0.1')
+  })
+
+  it('compound edge case', () => {
+    expect(numSanitize('--00123', {
+      isMinusAllowed: true
+    })).toBe('-123')
+
+    expect(numSanitize('00..123', { 
+      isDotAllowed: true, 
+    })).toBe('0.123')
+
+    expect(numSanitize('--00..1..23--', { 
+      isMinusAllowed: true,
+      isDotAllowed: true, 
+      digits: 2
+    })).toBe('-0.12')
+
+    expect(numSanitize('000.23', { 
+      isMinusAllowed: true,
+      isDotAllowed: true, 
+      digits: 2
+    })).toBe('0.23')
+
+    expect(numSanitize('01.23', { 
+      isMinusAllowed: true,
+      isDotAllowed: true, 
+      digits: 2
+    })).toBe('1.23')
+
+    expect(numSanitize('-001.23', { 
+      isMinusAllowed: true,
+      isDotAllowed: true, 
+      digits: 2
+    })).toBe('-1.23')
+  })
+  
+})
+
+
+
+
 
