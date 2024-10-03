4e2cb3e10e743ca0b4e6a2b879d502c45b46804f
Naxisigut <834525217@qq.com>
2024-10-03 23:50:47 +0800

    feat: web utils

---
+++ test/web.test.ts
@@ -0,0 +1,10 @@
+import { test, expect } from 'vitest';
+import { getWebCookie, copyToClipboard } from '../src/web/index';
+
+test('getWebCookie', () => {
+  // expect(getWebCookie('abc')).toBe(undefined)   
+})
+
+test('copyToClipboard', () => {
+  // expect(copyToClipboard('abc')).toBe(undefined)   
+})
